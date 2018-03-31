532c3e27cbdea0f452cdb7c4492ddfce68b7285a
Purple Myst <PurpleMyst@users.noreply.github.com>
2018-03-31 17:14:50 +0200

    Add a factorial function

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,21 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#[macro_use] extern crate cpython;
+
+use cpython::{PyResult, Python};
+
+py_module_initializer!(libfactorial_rs, initlibfactorial_rs, PyInit_libfactorial_rs, |py, module| {
+    module.add(py, "__doc__", "Tail-recursive factorial written in Rust.")?;
+    module.add(py, "factorial", py_fn!(py, factorial_py(n: u64)))?;
+    Ok(())
+});
+
+fn factorial(n: u64, acc: u64) -> u64 {
+    if n == 0 {
+        acc
+    } else {
+        factorial(n - 1, n * acc)
     }
+}
+
+fn factorial_py(_: Python, n: u64) -> PyResult<u64> {
+    Ok(factorial(n, 1))
 }
